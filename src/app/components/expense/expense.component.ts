6d231becf810ccca0e3911f471261e9f088ed80a
void-rahul <rahuljagtapemail@gmail.com>
2023-01-01 15:17:34 +0530

    Card

--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./expense.component.css']
 })
 export class ExpenseComponent {
+  isSidePanelOpen: boolean = false;
+
+
+
+
+
+
+
+
+  addSidePanel(){
+    this.isSidePanelOpen= true;
+   }
+   cloeSidePanel(){
+    this.isSidePanelOpen= false;
+   }
+
 
 }
